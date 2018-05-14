57676084f64ec660b5e5aae59bff63bd2530e534
Kailin Cannon <kailin.cannon@gmail.com>
2018-05-14 16:04:08 -0600

    Server setup done

---
+++ db/migrations/20180514154504_initial.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('muscle_groups', function(table) {
+      table.increments('id').primary();
+      table.string('muscle_group');
+      table.string('targeted_area');
+      table.string('train_with');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('exercises', function(table) {
+      table.increments('id').primary();
+      table.string('exercise');
+      table.string('level');
+      table.string('method');
+      table.string('upper_lower_core');
+      table.string('joint');
+      table.integer('muscle_group_id').unsigned();
+      table.foreign('muscle_group_id')
+        .references('muscle_groups.id')
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('exercises');
+    knex.schema.dropTable('muscle_groups');
+  ])
+};
