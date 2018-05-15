acda5236a2ba62c67eccd20a6a3c5c1f33982d94
Aldo Mercado <amercado1014@gmail.com>
2018-05-15 16:49:41 -0600

    Add get endpoints for exercises and muscle groups

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'BYOB';
 
+app.get('/api/v1/muscle-groups', (request, response) => {
+  database('muscle_groups').select()
+    .then(muscleGroups => response.status(200).json(muscleGroups))
+    .catch(error => response.status(500).json(error))
+});
+
+app.get('/api/v1/exercises', (request, response) => {
+  database('exercises').select()
+    .then(exercises => response.status(200).json(exercises))
+    .catch(error => response.status(500).json(error))
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
