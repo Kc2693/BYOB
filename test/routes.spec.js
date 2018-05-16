39bd5d561d227ac9124846cd23de3a6aad910875
Aldo Mercado <amercado1014@gmail.com>
2018-05-16 17:32:54 -0600

    Add get exercises by id tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -121,4 +121,44 @@ describe('API Routes', () => {
         done();
       });
   });
+
+  it('GET exercises by id should return a single exercise', done => {
+    const exerciseId = 1;
+
+    chai.request(server)
+      .get(`/api/v1/exercises/${exerciseId}`)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.an('array');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('exercise');
+        response.body[0].exercise.should.equal('Full Reverse Crunch');
+        response.body[0].should.have.property('level');
+        response.body[0].level.should.equal('Advanced');
+        response.body[0].should.have.property('method');
+        response.body[0].method.should.equal('FW');
+        response.body[0].should.have.property('upper_lower_core');
+        response.body[0].upper_lower_core.should.equal('Core');
+        response.body[0].should.have.property('joint');
+        response.body[0].joint.should.equal('M');
+        done();
+      });
+  });
+
+  it('GET exercises by id should return an error if exercise is not found', done => {
+    const exerciseId = 5;
+
+    chai.request(server)
+      .get(`/api/v1/exercises/${exerciseId}`)
+      .end((err, response) => {
+        response.should.have.status(404);
+        response.should.be.json;
+        response.body.should.have.property('error');
+        response.body.error.should.equal('Could not find exercise with id 5');
+        done();
+      });
+  });
 }); 
